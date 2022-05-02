92e3bb3256322c8ab9826281f7f268cf4ebdb8d7
jonnyhoudini <jmcaveety@gmail.com>
2022-05-02 20:18:31 +0100

    first commit

---
+++ precourse_recap.py
@@ -0,0 +1,16 @@
+name = "jonny"
+
+age = 33
+
+hours_of_homework = 2.5
+
+name_cap = name.capitalize()
+
+if age < 40:
+    print("You're still youngish!")
+
+weekly_homework = hours_of_homework * 3
+
+def say_hello(name):
+    print("Hello", name_cap)
+
